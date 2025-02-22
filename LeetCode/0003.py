8c07056fd2d74b3b0a103b39b9b5de0ddc4fcfc8
pagiguntur <guntur.pagi@gmail.com>
2025-02-22 22:25:20 +0700

    add new solution leetcode

---
+++ LeetCode/0003.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        maxLength = 0
+        charIndex = [-1] * 128
+        left = 0
+        
+        for right in range(n):
+            if charIndex[ord(s[right])] >= left:
+                left = charIndex[ord(s[right])] + 1
+            charIndex[ord(s[right])] = right
+            maxLength = max(maxLength, right - left + 1)
+        
+        return maxLength
---
+++ LeetCode/0867.py
@@ -0,0 +1,8 @@
+        # slow_pointer = head
+        # fast_pointer = head
+
+        # while fast_pointer is not None and fast_pointer.next is not None:
+        #     slow_pointer = slow_pointer.next
+        #     fast_pointer = fast_pointer.next.next
+
+        # return slow_pointer
